d2b2e399b1656e1e83ff5a0c9de36dd6e1823b17
Reuben Gardos Reid <ReubenJ@users.noreply.github.com>
2021-07-09 15:26:48 -0500

    add twitter timeline to splash page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
 import MallardLogo from '../components/logo';
+import { Timeline } from 'react-twitter-widgets';
 
 export default function Index() {
   return (
-    <div>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div style={{ display: 'flex', alignItems: 'center', height: '90vh', width: '100vw', flexDirection: 'column', justifyContent: 'space-around'}}>
         <MallardLogo />
+        <Timeline
+          dataSource={{
+            sourceType: 'profile',
+            screenName: 'MPLSmallard'
+          }}
+          options={{
+            height: '60vh',
+            // width: '500'
+          }} />
       </div>
-    </div>
   );
 }

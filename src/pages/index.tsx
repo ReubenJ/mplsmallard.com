3e2ca0fbd7292f8ace0dae8979ebb526ad8f2249
Reuben Gardos Reid <ReubenJ@users.noreply.github.com>
2021-07-09 15:32:46 -0500

    update head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import MallardLogo from '../components/logo';
 import { Timeline } from 'react-twitter-widgets';
+import Head from "next/head";
 
 export default function Index() {
   return (
       <div style={{ display: 'flex', alignItems: 'center', height: '90vh', width: '100vw', flexDirection: 'column', justifyContent: 'space-around'}}>
+        <Head>
+          <title>Mallard Ultimate</title>
+          <meta name="description" content="Mallard is an open club ultimate team from Minneapolis."/>
+        </Head>
         <MallardLogo />
         <Timeline
           dataSource={{
